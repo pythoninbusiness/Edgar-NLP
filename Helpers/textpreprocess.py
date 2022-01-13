b1f1d66b8b4bdb1d82e6ebc85328f1fe32bb4196
pythoninbusiness <90924851+pythoninbusiness@users.noreply.github.com>
2022-01-13 10:50:54 -0800

    Update textpreprocess.py
    added lemma and stop word removal

--- Helpers/textpreprocess.py
+++ Helpers/textpreprocess.py
@@ -1,10 +1,7 @@
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
 from bs4 import BeautifulSoup
 from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-from typing import List
 from . import load
 import pandas as pd
 import unicodedata
@@ -45,11 +42,35 @@ def remove_extra_spaces(list_of_documents):
     return list_of_clean_docs
 
 
+def lemmatize(list_of_documents):
+    list_of_cleaned_documents = list()
+    lemmatizer = WordNetLemmatizer()
+    for doc in list_of_documents:
+        dirty_tokens = doc.split(" ")
+        cleaned_tokens = [ lemmatizer.lemmatize(word) for word in dirty_tokens if len(word) > 1]
+        cleaned_sentence = " ".join(cleaned_tokens)
+        list_of_cleaned_documents.append(cleaned_sentence)
+    return list_of_cleaned_documents
+
+
+def stop_word_removal(list_of_documents):
+    list_of_cleaned_documents = list()
+    stop_words = stopwords.words('english')
+    for doc in list_of_documents:
+        dirty_tokens = doc.split(" ")
+        cleaned_sentence = " ".join( [token for token in dirty_tokens if token not in stop_words] )
+        list_of_cleaned_documents.append(cleaned_sentence)
+    
+    return list_of_cleaned_documents
+
+
 def typical_preprocess(list_of_documents):
     list_of_clean_docs = remove_digits(list_of_documents)
     list_of_clean_docs = lowercase(list_of_clean_docs)
     list_of_clean_docs = remove_punctuation(list_of_clean_docs)
     list_of_clean_docs = remove_extra_spaces(list_of_clean_docs)
+    list_of_clean_docs = lemmatize(list_of_clean_docs)
+    list_of_clean_docs = stop_word_removal(list_of_clean_docs)
     list_of_clean_docs = [doc.strip() for doc in list_of_clean_docs]
     return list_of_clean_docs
 
@@ -70,30 +91,6 @@ def split_into_sentences(text, min_words=5):
     return sentences
 
 
-def lemmatize_sentences_and_remove_stop_words(list_of_sentences, additional_stop_words=[], remove_digits=False):
-    lemmatizer = WordNetLemmatizer()
-    custom_stop_words = set(stopwords.words('english') + additional_stop_words)
-
-    cleaned_sentences = list()
-
-    for sentence in list_of_sentences:
-        if remove_digits:
-            sentence = re.sub(r"\d(\.)\d", " ", sentence)
-            sentence = re.sub(r"(\d)+", "", sentence)
-        dirty_tokens = sentence.split(" ")
-        cleaned_tokens = [ lemmatizer.lemmatize(word.lower().strip()) for word in dirty_tokens if len(word) > 1]
-        cleaned_tokens = [ word for word in cleaned_tokens if word not in custom_stop_words ]
-        cleaned_sentence = " ".join(cleaned_tokens)
-        
-        exclude_punctuation = string.punctuation
-        exclude_table = str.maketrans("", "", exclude_punctuation)
-        cleaned_sentence = cleaned_sentence.translate(exclude_table)
-        
-        cleaned_sentences.append(cleaned_sentence)
-
-    return cleaned_sentences
-
-
 def turn_filing_into_sentences_df(report_link):
     """ 
     Purpose: Turn an SEC filing into a dataframe of dirty and clean sentences.
@@ -102,7 +99,7 @@ def turn_filing_into_sentences_df(report_link):
     entry_soup = load.load_soup_from_file_or_edgar(report_link)
     full_text = get_text_from_soup(entry_soup)
     sentences = split_into_sentences(full_text)
-    cleaned_sentences = lemmatize_sentences_and_remove_stop_words(sentences, remove_digits=True)
+    cleaned_sentences = typical_preprocess(sentences)
     report_df = pd.DataFrame( zip(sentences, cleaned_sentences), columns=["Dirty", "Clean"])
     
     return report_df
